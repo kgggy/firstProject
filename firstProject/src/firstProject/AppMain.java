67091ef4a613d013c92d92ca3b696a0702545f69
Admin <Admin@DESKTOP-U80CIQ7>
2021-06-23 14:35:18 +0900

    첫번째

---
+++ firstProject/src/firstProject/AppMain.java
@@ -0,0 +1,15 @@
+package firstProject;
+
+public class AppMain {
+//	클래스명은 항상 대문자로 작성.
+	
+//	 주석문: 프로그램 실행과는 관계없이 실행.
+	public static void main(String[] args) {
+//		 주석문: 프로그램 실행과는 관계없이 실행.
+		int age = 25;
+		String name = "권가영";
+	    System.out.println("내 이름은 : " + name );
+		System.out.println("내 나이는 : " + age);
+	}
+	
+}
---
+++ firstProject/src/firstProject/NumberExample.java
@@ -0,0 +1,11 @@
+package firstProject;
+
+public class NumberExample {
+	public static void main(String[] args) {
+		byte math = 100; // -128 ~ 127
+		byte eng = 90;
+		
+		int sum = math + eng; 
+		System.out.println("두 점수의 합 : "+ sum);
+		}
+}
