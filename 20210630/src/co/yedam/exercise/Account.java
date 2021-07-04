307192f73d4561b60bc0edbdb58ae4870c8d2891
sec <sec@DESKTOP-4IB0IH1>
2021-07-05 00:09:30 +0900

    7/2 homework

---
+++ 20210630/src/co/yedam/exercise/Account.java
@@ -0,0 +1,42 @@
+package co.yedam.exercise;
+
+public class Account {
+	private String ano;
+	private String owner;
+	private int balance;
+	
+	public Account(String ano, String owner, int balance) {
+		this.ano = ano;
+		this.owner = owner;
+		this.balance = balance;
+	}
+
+	public String getAno() {
+		return ano;
+	}
+
+	public void setAno(String ano) {
+		this.ano = ano;
+	}
+
+	public String getOwner() {
+		return owner;
+	}
+
+	public void setOwner(String owner) {
+		this.owner = owner;
+	}
+
+	public int getBalance() {
+		return balance;
+	}
+
+	public void setBalance(int balance) {
+		this.balance = balance;
+	}
+	public String showInfo() {
+		String info = "계좌번호: " + getAno() + " 계좌주: " + getOwner() + " 금액: " + getBalance();
+		return info; 
+	}
+
+}
